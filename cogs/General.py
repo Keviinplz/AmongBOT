2e883b702e4fe14a8adcad52b2755ce4154dffb8
Kevin Pinochet Hernández <keviinplz@DESKTOP-T6BFMME.localdomain>
2020-09-15 22:59:09 -0300

    Added new command  and update  list

--- cogs/General.py
+++ cogs/General.py
@@ -5,12 +5,20 @@ import discord
 class General(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+	
+	async def print_list(self, ctx, list_people, title, description):
+		names_to_str_builder = ""
+		for names in list_people:
+			names_to_str_builder += names + '\n'
+		names_to_str_builder += '\n' + description + '\n'
+		e = discord.Embed(title=title, type="rich", description=names_to_str_builder, colour=discord.Colour(0x09c48c))
+		await ctx.send('', embed=e)
 
 	@commands.command(name="room", help="Usage: $room <code> <server> -> Broadcast an available room, example: $room XFDS Europa")
 	async def room(self, ctx, code, server):
 		voice = ctx.message.author.voice
 		if voice == None:
-			e = discord.Embed(title=f':x: You must to be in a Voice Channel \n', colour=discord.Colour(0xcc1818))
+			e = discord.Embed(description=f':x: You must to be in a Voice Channel \n', colour=discord.Colour(0xcc1818))
 			await ctx.send('', embed=e)
 			return
 		voice_name = ctx.message.author.voice.channel.name
@@ -23,14 +31,16 @@ class General(commands.Cog):
 
 	@commands.command(name="funas", help="Usage: $funas -> Print the better five Impostor from this server")
 	async def funas(self, ctx):
-		funades = ["Pia 'muchotexto'", "Bryan 'ArAmIx'", "Javier 'koga'", "Catalina 'catasinbri'", "Kevin 'rojo'"]
-		title = ":eyes: Funades list:"
-		people_list = ""
-		for names in funades:
-			people_list += names + " \n"
-		people_list += "\n Congrats to be the best five Impostor of the week! :fire:"
-		e = discord.Embed(title=title, type="rich", description=people_list, colour=discord.Colour(0x09c48c))
-		await ctx.send('', embed=e)
-			
+		cancel_list = ["Javier 'koga'", "Brayan 'L'", "Bastian 'Basti'", "Catalina 'catasinbri'", "Melanie 'melanie'"]
+		title = "Best Impostors of the Week"
+		description = ":fire: Congrats y'all"
+		self.print_list(ctx, cancel_list, title, description)
+	
+	@commands.command(name="honor", help="Usage: $honor -> Print honorific people of this server")
+	async def honor(self, ctx):
+		honor_list = ["Pía 'muchotexto'", "Javier 'koga'", "Brayan 'ArAmIx'", "Kevin 'rojo'", "Catalina 'catasinbri'"]
+		title = "Hall of Honor"
+		description = ":love: Honorific people of the server"
+		self.print_list(ctx, honor_list, title, description)
 def setup(bot):
 	bot.add_cog(General(bot))
